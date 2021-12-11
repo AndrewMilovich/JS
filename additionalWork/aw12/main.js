1dda19fab951bacf73da6abc5b20c96cbb0a7a42
AndrewMilovich <triplle@ukr.net>
2021-12-11 20:35:25 +0200

    hw12 cw12 aw12 done

--- additionalWork/aw12/main.js
+++ additionalWork/aw12/main.js
@@ -3,24 +3,47 @@
 //     кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
 //     Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
 fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(value => value.json())
-    .then(value => {
-        for (const user of value) {
-            let div = document.createElement('div');
+    .then(user => user.json())
+    .then(users => {
+        for (const user of users) {
+            let divUser = document.createElement('div');
             let h = document.createElement('h2');
-            h.innerText = user.name;
-            let button = document.createElement("button");
-            button.innerText = 'Show Post User'
-            let divAddress = document.createElement("div");
-            let street = document.createElement("div");
-            street.innerText=user.address.street;
+            h.innerHTML = `${user.id} - ${user.name} - ${user.email}`;
+            let buttonPost = document.createElement("button");
+            buttonPost.innerText = 'Show Post User';
+            buttonPost.onclick = function (id) {
+                fetch('https://jsonplaceholder.typicode.com/users/' + user.id + '/posts')
+                    .then(post => post.json())
+                    .then(posts => {
+                        for (const post of posts) {
+                            let divPost = document.createElement(`div`);
+                            divPost.innerHTML = ` <h3>POST id:${post.id}</h3>
+<h4>title:${post.title}</h4>
+<h5>body:${post.body}</h5>`;
+                            let buttonComments = document.createElement("button");
+                            buttonComments.innerText = 'show comments';
+                            divUser.append(divPost, buttonComments);
+                            buttonComments.onclick = function (id) {
+                                fetch('https://jsonplaceholder.typicode.com/users' + user.id + '/posts' + post.id + `/comments`)
+                                    .then(comment => comment.json())
+                                    .then(comments => {
+                                        for (const comment of comments) {
+                                            if (post.id === comment.postId) {
+                                                let divComment = document.createElement('div');
+                                                divComment.innerHTML = `<h3>COMMENT ID:${comment.id}</h3>
+<h4> name:${comment.name}</h4>
+<h5>email:${comment.email}</h5>
+<h6>body:${comment.body}</h6>`;
+                                                divPost.appendChild(divComment)
+                                            }
+                                        }
+                                    })
+                            }
+                        }
+                    })
+            }
+            divUser.append(h, buttonPost);
+            document.body.append(divUser);
 
-button.onclick=function () {
-
-}
-divAddress.append(street);
-            div.append(h,divAddress);
-            document.body.append(div);
         }
-
     })
--- classWork/cw12/main.js
+++ classWork/cw12/main.js
@@ -9,16 +9,31 @@ fetch(`https://jsonplaceholder.typicode.com/posts`)
             div.classList.add('styleUsers');
             let title = document.createElement('h2');
             title.innerText = `${post.id} ${post.title}`;
+            let p = document.createElement('p');
+            p.innerText = post.body;
             let button = document.createElement('button');
             button.innerText = 'Show Post';
             button.style.margin = '20px';
-            let p = document.createElement('p');
-            div.append(title, button,p);
-            button.onclick = function () {
+            button.onclick = function (id) {
+                fetch('https://jsonplaceholder.typicode.com/posts/' + post.id + '/comments')
+                    .then(comment => comment.json())
+                    .then(comments => {
+                        for (const comment of comments) {
+                            if (post.id === comment.postId) {
+                                let divComment = document.createElement("div");
+                                divComment.innerHTML = `<h3>ID:${comment.id}</h3>
+<h4>name:${comment.name}</h4>
+<h5>email:${comment.email}</h5>
+<h6>body:${comment.body}</h6>`;
+                                div.appendChild(divComment)
+                            }
 
-                p.innerText = post.body;
+                        }
 
+
+                    })
             }
+            div.append(title, p, button);
             document.body.append(div);
         }
     })
